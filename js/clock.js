1f846955ba0f6752dfcfecd372053613ba8a05fb
leejungho-homa <leejho1107@naver.com>
2023-01-21 17:31:37 +0900

    2023.01.21
    my-page

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,23 @@
-function clock() {
-  const clockhtml = document.querySelector(".clock");
+const clockhtml = document.querySelector(".clock");
+const datehtml = document.querySelector(".dateclock");
+
+function dateclock() {
   const date = new Date();
-  const month = String(date.getMonth()).padstart(2, "0");
-  const hours = String(date.getHours()).padstart(2, "0");
-  const minutes = String(date.getMinutes()).padstart(2, "0");
-  const seconds = String(date.getSeconds()).padstart(2, "0");
-  clockhtml.innerHTML = `${month}:${hours}:${minutes}:${seconds}:`;
+  const years = String(date.getFullYear()).padStart(4, "0");
+  const months = String(date.getMonth() + 1).padStart(2, "0");
+  const days = String(date.getDate()).padStart(2, "0");
+  datehtml.innerHTML = `${years}/${months}/${days}`;
 }
+function clock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clockhtml.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
 function clockinit() {
+  dateclock();
   clock();
   setInterval(clock, 1000);
 }
